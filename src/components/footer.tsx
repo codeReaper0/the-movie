dba0f821e992e7696ed69406b2c44033f5c9176d
codeReaper0 <boluwatifetella@gmail.com>
2023-09-15 00:22:08 +0100

    fixed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import {
   FacebookIcon,
   InstagramIcon,
@@ -10,15 +11,29 @@ export default function Footer() {
   return (
     <footer className="gap-6 py-6 flex flex-col items-center">
       <div className="flex gap-6 md:gap-12">
-        <FacebookIcon />
-        <InstagramIcon />
-        <TwitterIcon />
-        <YoutubeIcon />
+        <Link href="#">
+          <FacebookIcon />
+        </Link>
+        <Link href="#">
+          <InstagramIcon />
+        </Link>
+        <Link href="#">
+          <TwitterIcon />
+        </Link>
+        <Link href="#">
+          <YoutubeIcon />
+        </Link>
       </div>
       <div className="flex gap-6 md:gap-12 px-4 lg:px-0">
-        <p className="text-[#111827] font-bold">Conditions of Use</p>
-        <p className="text-[#111827] font-bold">Privacy & Policy</p>
-        <p className="text-[#111827] font-bold">Press Room</p>
+        <Link className="text-[#111827] font-bold" href="#">
+          Conditions of Use
+        </Link>
+        <Link className="text-[#111827] font-bold" href="#">
+          Privacy & Policy
+        </Link>
+        <Link className="text-[#111827] font-bold" href="#">
+          Press Room
+        </Link>
       </div>
       <p className="text-[#6B7280] font-bold">
         © 2023 MovieBox by Tella Boluwatife
